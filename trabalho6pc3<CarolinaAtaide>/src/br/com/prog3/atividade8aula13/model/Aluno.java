df719cb8af862f59189664d26f6edf0b0fbdbff3
Carol Ataíde <30929945+sknyx@users.noreply.github.com>
2017-12-10 18:08:43 -0200

    Add files via upload

---
+++ trabalho6pc3<CarolinaAtaide>/src/br/com/prog3/atividade8aula13/model/Aluno.java
@@ -0,0 +1,33 @@
+package br.com.prog3.atividade8aula13.model;
+
+public class Aluno {
+	Integer matricula;
+	String nome;
+	String ra;
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ trabalho6pc3<CarolinaAtaide>/src/br/com/prog3/atividade8aula13/persistence/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package br.com.prog3.atividade8aula13.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection() {
+		String driver = "org.postgresql.Driver";
+		String user = "postgres";// Coloque o usuário criado para acesso ao banco
+		String senha = "prog3ifb";// Coloque a senha para acesso ao banco
+		String url = "jdbc:postgresql://127.0.0.1:5432/aula13";// Coloque o
+		// servidor onde está instalado o banco
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = (Connection) DriverManager.getConnection(url, user, senha);
+			//System.out.println("Conexão realizada com sucesso.");
+		} catch (ClassNotFoundException ex) {
+			System.err.print(ex.getMessage());
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+		}
+		return con;
+	}
+
+	public static void close(Connection con) {
+		try {
+			con.close();
+		} catch (Exception e) {
+		}
+	}
+}
